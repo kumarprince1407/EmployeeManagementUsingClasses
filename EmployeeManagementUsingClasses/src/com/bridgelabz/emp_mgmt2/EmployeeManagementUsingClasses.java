971a84d0ac37b91b9ddb8ff527a55cae326938be
Prince Kumar <kumarprince1407@gmail.com>
2023-06-26 18:32:50 +0530

    Initial commit

---
+++ EmployeeManagementUsingClasses/src/com/bridgelabz/emp_mgmt2/EmployeeManagementUsingClasses.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.emp_mgmt2;
+
+public class EmployeeManagementUsingClasses {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Management using classes.");
+    }
+}
